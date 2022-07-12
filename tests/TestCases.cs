4676e18e2cfa68ae50f8b1f653e2f1d8c165935e
sharanabasappa <sharanpadashetty637@gmail.com>
2022-07-12 13:17:27 +0530

    Resttucture

--- tests/TestCases.cs
+++ tests/TestCases.cs
@@ -27,77 +27,55 @@ namespace Amazon
             lp.clickOnSigninButton();
 
             var actualName = driver.FindElement(By.XPath("//div[@class='nav-line-1-container']/span[@id='nav-link-accountList-nav-line-1']"));
-            Assert.AreEqual("Hello, sharanabasappa", actualName.Text); ;
-
-
-
+            Assert.AreEqual("Hello, sharanabasappa", actualName.Text); 
         }
 
         [Test, Order(2)]
-        public void productSearch()
+        public void prodSearch()
 
-        {
-
-            ItemSearch itemSearch = new ItemSearch(getDriver());
-            itemSearch.searchItem("Samsung Galaxy M53 5G");
-            itemSearch.clickOnSearchbutton();
+        {   ProductSearch ps = new ItemSearch(getDriver());
+            ps.searchItem("Samsung Galaxy M53 5G");
+            ps.clickOnSearchbutton();
           
-            String atext = itemSearch.getText().Text;
+            String atext = ps.getText().Text;
             String[] splittedText = atext.Split("(");
             String trimmedAText = splittedText[0].Trim();
 
-            itemSearch.clickOnSearchbutton();
+            //itemSearch.clickOnSearchbutton();
             Assert.AreEqual("Samsung Galaxy M53 5G", trimmedAText);
 
-
-
         }
+       
         [Test, Order(3)]
-
         public void productAddedintoTheCart()
 
         {
-
             String parentWindow = driver.CurrentWindowHandle;
             driver.FindElement(By.XPath("//div[contains(@class,'rush-component s-featured-result-item')]//span[@class='a-size-medium a-color-base a-text-normal'][contains(text(),'Samsung Galaxy M53 5G (Mystique Green, 6GB, 128GB ')]")).Click();
-
-
             String childWindow = driver.WindowHandles[1];
             driver.SwitchTo().Window(childWindow);
-            addToCart atc = new addToCart(getDriver());
-            atc.selecttQuantity();
-            atc.clickonAddToCart();
+            ProductDescription pp = new ProductDescription(getDriver());
+            pd.selecttQuantity();
+            pd.clickonAddToCart();
             driver.SwitchTo().Window(parentWindow);
             Assert.AreEqual(2, driver.WindowHandles.Count);
         }
 
         [Test, Order(4)]
-
         public void goBackToCart()
-
         {
-            HomeCartPage hcp = new HomeCartPage(getDriver());
-            hcp.backToCart();
+            NavigationPage np = new NavigationPage(getDriver());
+            np.backToCart();
+            np.footer();
             Assert.IsTrue(driver.FindElement(By.CssSelector("a[id='nav-cart']")).Enabled);
-
         }
-
+        
         [Test,Order(5)]
-
-        public void scrollUptoFooter()
-        {
-            scrollDown sd = new scrollDown(getDriver());
-            sd.footer();
-         
-        }
-
-        [Test,Order(6)]
-
         public void clickOnRadioButtons()
         {
             driver.SwitchTo().DefaultContent();
-            RadioButtons rd = new RadioButtons(getDriver());
-            rd.radioButton();
+            LangPage lpp = new LangPage(getDriver());
+            lpp.radioButton();
             Assert.IsTrue(driver.FindElement(By.XPath("//div[@class='a-radio a-radio-fancy']/label/input[@value='kn_IN']")).Selected);
         }
 
