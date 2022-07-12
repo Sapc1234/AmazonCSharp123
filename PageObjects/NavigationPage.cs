9d587088fff803434f7d3c38c40de80c289f7dc4
sharanabasappa <sharanpadashetty637@gmail.com>
2022-07-12 13:18:13 +0530

    Restructure

---
+++ PageObjects/NavigationPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Amazon.PageObjects
+{
+    public class NavigationPage
+    {
+        private By homePageCart = By.CssSelector("a[id='nav-cart']");
+        private By clickonAmazonLogo = By.XPath("//div[@id='nav-logo']/a");
+        private By cScroll = By.XPath("//div[@class='navFooterLine']");
+        private By selectLang = By.XPath("//a[@id='icp-touch-link-language']");
+
+        public IWebDriver driver;
+       
+        public NavigationPagePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void backToCart()
+        {
+            driver.FindElement(homePageCart).Click();
+            Thread.Sleep(5000);
+            driver.FindElement(clickonAmazonLogo).Click();
+        }
+
+        public void footer()
+        {
+            IWebElement cs = driver.FindElement(cScroll);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", cs);
+            Thread.Sleep(5000);
+            Actions a1 = new Actions(driver);
+            a1.MoveToElement(driver.FindElement(selectLang)).Click().Perform();
+
+           // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+           // wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(selectLang));
+        }
+    }
+}
---
+++ PageObjects/ProductDescription.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Amazon.PageObjects
+{
+    public class ProductDescription
+    {
+        private By selectquantity = By.XPath("//select[@id='quantity']");
+        private By clickaddToCart = By.XPath("//input[@name='submit.add-to-cart']");
+        
+        public IWebDriver driver;
+       
+        public ProductDescription(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void selecttQuantity()
+        {
+            IWebElement staticdropDown = driver.FindElement(selectquantity);
+            SelectElement sd = new SelectElement(staticdropDown);
+            sd.SelectByValue("3");
+        }
+
+        public void clickonAddToCart()
+        {
+            driver.FindElement(clickaddToCart).Click();
+        }
+
+    }
+}
